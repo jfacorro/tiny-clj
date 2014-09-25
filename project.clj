037d35dbc94ee0b3c35f0aef2288450b57948096
Juan Facorro <juan.facorro@gmail.com>
2014-09-25 00:31:54 -0300

    Added min lein version.

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [hiccup "1.0.5"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [environ "0.5.0"]]
+  :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "tiny-clj-standalone.jar"
